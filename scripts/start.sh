0caf662619df6d0e92c827e996e0c21aee2008e2
richardstartin <richard@openkappa.co.uk>
2020-12-14 18:28:39 +0000

    streaming

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,16 +2,25 @@
 OPTIONS=""
 if [ "$1" == "inlining" ];
 then
-  OPTIONS="-XX:+UnlockDiagnosticVMOptions -XX:+PrintInlining -ms2G -mx2G   -javaagent:../../dd-trace-java/dd-java-agent/build/libs/dd-java-agent-$2.jar -Ddd.trace.enabled=true"
+  OPTIONS="-XX:+UnlockDiagnosticVMOptions -XX:+PrintInlining -ms2G -mx2G   -javaagent:../../dd-trace-java/dd-java-agent/build/libs/dd-java-agent-$2.jar -Ddd.trace.enabled=true "
 fi
 
+if [ "$1" == "ap" ];
+then
+  OPTIONS="-ms2g -mx2g -agentpath:/home/richard/async-profiler-1.8.1-linux-x64/build/libasyncProfiler.so=start,event=alloc,file=alloc-$2.svg -javaagent:../../dd-trace-java/dd-java-agent/build/libs/dd-java-agent-$2.jar -Ddd.trace.enabled=true -Ddd.trace.health.metrics.enabled=false -Ddd.trace.perf.metrics.enabled=false"
+fi
 
 if [ "$1" == "cpu" ];
 then
-  OPTIONS="-XX:+AlwaysPreTouch -ms2G -mx2G -javaagent:../../dd-trace-java/dd-java-agent/build/libs/dd-java-agent-$2.jar -Ddd.trace.enabled=true"
+  OPTIONS="-ms2G -mx2G -javaagent:../../dd-trace-java/dd-java-agent/build/libs/dd-java-agent-$2.jar -Ddd.trace.enabled=true"
+fi
+
+if [ "$1" == "gc" ];
+then
+  OPTIONS="-ms2G -mx2G -verbose:gc -javaagent:../../dd-trace-java/dd-java-agent/build/libs/dd-java-agent-$2.jar -Ddd.trace.enabled=true"
 fi
 
 
 $JAVA_HOME/bin/java ${OPTIONS} \
-    -DnbThreads=200 \
+    -DnbThreads=4 \
     -jar ../target/spring-petclinic-2.2.0.BUILD-SNAPSHOT.jar > out_petclinic.txt
